1684259ee7c0006600fbe14037ff0e6e4647495d
liutaochange <liutaochange@163.com>
2018-09-09 17:43:04 +0800

    modify file directory

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# less-study
+### less-study
+--------
+
+#### 变量 (Variables)
+
+> less 中变量的生命通过@来实现， @变量名：值；
+
+```
+@width: 300px;
+@bg-color: #5B83AD;
+@font-color: #fff;
+
+.box{
+  width: @width;
+  background: @bg-color;
+  color: @font-color;
+}
+
+```
+
+#### 混合 (Mixins)
+
 less deep learning and summary
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,40 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var plumber = require('gulp-plumber');
-
+var connect = require('gulp-connect');
+/**
+ * 声明文件路径
+ */
 var paths = {
-	less: ['./**/less/*.less','!./node_modules','!./node_modules/**','!./node_modules/**/*.less']
+	less: ['./src/**/*.less','!./node_modules','!./node_modules/**','!./node_modules/**/*.less']
 };
+/**
+ * 定义编译less的任务
+ */
 gulp.task('less', function () {
-  return gulp.src(paths.less)
-  .pipe(plumber())
-	.pipe(less())
-	.pipe(gulp.dest('./dist/'));
+  gulp.src(paths.less)
+      .pipe(plumber())
+      .pipe(less())
+      .pipe(gulp.dest('./dist/'))
+      .pipe(connect.reload());
 });
-
-
-gulp.task('default', ['less'], function() {
-  // 将你的默认的任务代码放在这
-  console.log('success')
+/**
+ * 利用gulp启动本地服务，实时更新
+ */
+gulp.task('server', function() {
+  connect.server({
+    livereload: true,
+    port: 2333
+  });
 });
+/**
+ * 开启监听，文件有修改自动执行任务
+ */
+gulp.task('watch',function(){
+  gulp.watch('./src/**/*.less', ['less']);
+  gulp.watch('./index.html', ['less']);
+})
+/**
+ * 定义默认任务，命令行里输入gulp即可执行
+ */
+gulp.task('default', ['less','watch','server']);
